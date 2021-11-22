33cfa29a4fd9d5d7b24bddac123ba6d70e7b4078
paulwababu <44939805+paulwababu@users.noreply.github.com>
2021-11-22 19:10:57 +0300

    Update urls.py

--- covid19/urls.py
+++ covid19/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('api/upload', UploadView.as_view(), name = 'prediction'),
+    path('api/upload/xray', UploadView.as_view(), name = 'prediction'),
+    path('api/upload/ct', CTUploadView.as_view(), name = 'ct_prediction'),
 ]
