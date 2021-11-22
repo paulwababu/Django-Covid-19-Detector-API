bd5919c9ddedb0af11256197d2e766d980c4358a
paulwababu <44939805+paulwababu@users.noreply.github.com>
2021-11-22 19:11:32 +0300

    Update apps.py

--- covid19/apps.py
+++ covid19/apps.py
@@ -15,17 +15,37 @@ class ResNetModelConfig(AppConfig):
     MODEL_FILE = os.path.join(settings.MODELS, "resnet_chest.h5")
     model = keras.models.load_model(MODEL_FILE)
 
+class ResNetCTModelConfig(AppConfig):
+    name = 'resnetCTAPI'
+    MODEL_FILE = os.path.join(settings.MODELS, "resnet_ct.h5")
+    model = keras.models.load_model(MODEL_FILE)
+
 class VGGModelConfig(AppConfig):
     name = 'vggAPI'
     MODEL_FILE = os.path.join(settings.MODELS, "vgg_chest.h5")
     model = keras.models.load_model(MODEL_FILE)
+
+class VGGCTModelConfig(AppConfig):
+    name = 'vggCTAPI'
+    MODEL_FILE = os.path.join(settings.MODELS, "vgg_ct.h5")
+    model = keras.models.load_model(MODEL_FILE)    
 
 class InceptionModelConfig(AppConfig):
     name = 'inceptionv3_chestAPI'
     MODEL_FILE = os.path.join(settings.MODELS, "inceptionv3_chest.h5")    
     model = keras.models.load_model(MODEL_FILE)
 
+class InceptionCTModelConfig(AppConfig):
+    name = 'inceptionv3_chestCTAPI'
+    MODEL_FILE = os.path.join(settings.MODELS, "inception_ct.h5")    
+    model = keras.models.load_model(MODEL_FILE)    
+
 class ExceptionModelConfig(AppConfig):
     name = 'xception_chestAPI'
     MODEL_FILE = os.path.join(settings.MODELS, "xception_chest.h5")    
     model = keras.models.load_model(MODEL_FILE)
+
+class ExceptionCTModelConfig(AppConfig):
+    name = 'xception_chestCTAPI'
+    MODEL_FILE = os.path.join(settings.MODELS, "xception_ct.h5")    
+    model = keras.models.load_model(MODEL_FILE)    
